05b2f6561dc30427dcfb9c6925b865461471375e
Ben <bennrhodes@gmail.com>
2022-04-16 21:25:16 +1000

    progress

--- src/components/Progress/Progress.tsx
+++ src/components/Progress/Progress.tsx
@@ -1,10 +1,76 @@
-import React from 'react';
-import {StyledProgress} from './progress.style.ts';
+import classnames from 'classnames';
+import React, { useMemo } from 'react';
+import { StyledProgress } from './progress.style';
 
-export interface ProgressProps {}
-
-export const Progress: React.FC<ProgressProps> = ({ children }) => {
-  return (
-    <StyledProgress>{ children }</StyledProgress>
-  );
+export interface ProgressProps {
+  size?: number;
+  children?: React.ReactNode;
+  progress?: number;
+  active?: boolean | undefined;
+  disabled?: boolean | undefined;
+  strokeCap?: `${StrokeCap}`;
+  className?: string;
 }
+
+enum StrokeCap {
+  BUTT = 'flat',
+  ROUND = 'round'
+}
+
+const DEFAULT_SIZE = 120;
+const MIN_SIZE = 100;
+const STROKE_WIDTH = 12;
+
+export const Progress: React.FC<ProgressProps> = ({
+  children,
+  size = DEFAULT_SIZE,
+  progress = 30,
+  active,
+  disabled,
+  strokeCap = StrokeCap.ROUND,
+  className
+}) => {
+  const classes = classnames(
+    'progress',
+    active && `progress--active`,
+    disabled && `progress--disabled`,
+    className
+  );
+
+  const limitedSize = size < MIN_SIZE ? MIN_SIZE : size;
+  const limitedProgress = progress > 100 ? 100 : progress;
+  const radius = useMemo(() => Math.round(limitedSize / 2 - STROKE_WIDTH / 2), [limitedSize]);
+  const circumference = useMemo(() => radius * 2 * Math.PI, [radius]);
+  const dash = useMemo(() => circumference - (limitedProgress / 100) * circumference, [
+    circumference,
+    limitedProgress
+  ]);
+
+  const innerCircleSize = useMemo(() => Math.round(limitedSize - STROKE_WIDTH * 2), [limitedSize]);
+
+  return (
+    <StyledProgress
+      size={limitedSize}
+      dash={dash}
+      circumference={circumference}
+      strokeWidth={STROKE_WIDTH}
+      data-test='herma-progress'
+      innerCircleSize={innerCircleSize}
+      className={classes}
+    >
+      <svg className='progress progress--circular' width={limitedSize} height={limitedSize}>
+        <circle
+          className='progress__bar'
+          stroke='red'
+          strokeLinecap={strokeCap === StrokeCap.BUTT ? 'butt' : StrokeCap.ROUND}
+          strokeWidth={STROKE_WIDTH}
+          fill='transparent'
+          r={Math.round(radius)}
+          cx='50%'
+          cy='50%'
+        />
+      </svg>
+      <div className='progress__content'>{children}</div>
+    </StyledProgress>
+  );
+};
--- src/components/Progress/progress.style.ts
+++ src/components/Progress/progress.style.ts
@@ -1,5 +1,70 @@
 import styled from '@emotion/styled';
+import { css } from '@emotion/react';
+import { ProgressProps } from './Progress';
 
-export const StyledProgress = styled.button`
-  background: green;
+interface StyledProgressProps extends ProgressProps {
+  // theme: Theme;
+  dash: number;
+  circumference: number;
+  strokeWidth: number;
+  innerCircleSize: number;
+}
+
+const circular = ({
+  // variant,
+  // theme,
+  size,
+  innerCircleSize,
+  dash,
+  circumference,
+  strokeWidth
+}: StyledProgressProps) => {
+  const isCircular = true; //variant === ProgressBarVariants.CIRCULAR;
+
+  if (isCircular) {
+    return css`
+      margin: 0 auto;
+      width: ${size}px;
+      height: ${size}px;
+      background: blue;
+      border-radius: 50%;
+      position: relative;
+      .progress__bar {
+        transition: 0.35s stroke-dashoffset;
+        transform: rotate(-90deg);
+        transform-origin: 50% 50%;
+        stroke-dashoffset: ${dash};
+        stroke-dasharray: ${circumference} ${circumference};
+      }
+      .progress__content {
+        background: white;
+        width: ${innerCircleSize}px;
+        height: ${innerCircleSize}px;
+        top: ${strokeWidth}px;
+        left: ${strokeWidth}px;
+        border-radius: 50%;
+        position: absolute;
+        display: flex;
+        align-items: center;
+        justify-content: center;
+      }
+    `;
+  }
+  return '';
+};
+
+// eslint-disable-next-line arrow-body-style
+const horizontal = () => {
+  // const isHorizontal = variant === ProgressBarVariants.HORIZONTAL;
+  // if (isHorizontal) {
+  //   return css`
+  //     background: red;
+  //   `;
+  // }
+  return '';
+};
+
+export const StyledProgress = styled.div<StyledProgressProps>`
+  ${horizontal}
+  ${circular}
 `;
